6756b1c972915d1dab54b613db51895b67c792f4
carol011 <532473200@qq.com>
2017-06-17 21:30:46 +0800

    qqqqqqq

--- src/com/controller/OrderInfoController.java
+++ src/com/controller/OrderInfoController.java
@@ -1,8 +1,13 @@
 package com.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JsonConfig;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.convert.JsonDateValueProcessor;
+import com.pojo.OrderDetail;
 import com.pojo.OrderInfo;
 import com.service.IOrderInfoService;
 
@@ -21,23 +27,73 @@ public class OrderInfoController {
 	@Resource(name="orderInfoServiceImpl")
 	IOrderInfoService service;
 	@RequestMapping(value="addorder")
+	@ResponseBody
 	public String addOrder(OrderInfo order){	
-		service.save(order);
-		return null;
+		System.out.println(order.getOrderItem().size());
+		boolean save = service.save(order);
+		if(save){
+			return "[{frontid:"+order.getFrontid()+",orderid:"+order.getOrderid()+",}]";
+		}else{
+			return "[{frontid:"+order.getFrontid()+",}]";
+		}
+		
 		
 	}
 	
 	
 	@RequestMapping(value="showorderinfo")
-	@ResponseBody
-	public String showOrderInfo(Integer orderid){
+	public void showOrderInfo(Integer orderid,HttpServletResponse response){
 		OrderInfo order = service.findById(orderid);
-		System.out.println(order.getOrderid());
 		JsonConfig jsonConfig = new JsonConfig();  
 		jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor()); 
 		JSONArray array = new JSONArray();
 		array.add(order,jsonConfig);
-		
-		return array.toString();
+
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			out.write(array.toString());
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ;
+	}
+	
+	
+	@RequestMapping(value="showall")//fron
+	public void showOrderInfo(OrderInfo order,HttpServletResponse response){
+		System.out.println(order.getFrontid());
+		  List<OrderInfo> list = service.findByStatus(order);
+		JsonConfig jsonConfig = new JsonConfig();  
+		jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor()); 
+		JSONArray array = new JSONArray();
+		array.addAll(list, jsonConfig);
+
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			out.write(array.toString());
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ;
+	}
+	
+	
+	@RequestMapping(value="payOrder")
+	@ResponseBody
+	public String payOrder(OrderInfo or){
+		OrderInfo order = new OrderInfo();
+		order.setOrderid(or.getOrderid());
+		order.setOrderstatus(or.getOrderstatus());
+		boolean flag = service.update(order);
+		if(flag){
+			return "[{result:ok}]";
+		}
+		return "[{result:error}]";
 	}
 }
