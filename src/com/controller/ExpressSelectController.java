9153962014ce108cb778c22bc6541d7549cb7ad1
carol011 <532473200@qq.com>
2017-06-22 09:09:15 +0800

    修改express控制器

--- src/com/controller/ExpressSelectController.java
+++ src/com/controller/ExpressSelectController.java
@@ -9,16 +9,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONArray;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pojo.ExpressSelect;
 import com.service.IExpressSelectService;
-
+@Controller
+@RequestMapping("express")
 public class ExpressSelectController {
 	@Resource(name = "expressSelectServiceImpl")
 	IExpressSelectService eService;
 
-	@RequestMapping(value = "")
+	@RequestMapping(value = "showall")
 	public void showAll(HttpServletResponse response) {
 		List<ExpressSelect> findAll = eService.findAll();
 		JSONArray array = new JSONArray();
